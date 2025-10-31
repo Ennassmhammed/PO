8edfa45db015105949771b0a9525c842e2ee9714
Ennassmhammed <ennassirimohammed14@gmail.com>
2025-10-31 12:02:36 +0100

    cours

--- exercice1.php
+++ exercice1.php
@@ -5,7 +5,6 @@ class Personne {
    
     private $_nom;
     private $_prenom;
-    private $_age;
     private $_dateNaissance;
     private $_ville;
 
@@ -13,10 +12,43 @@ class Personne {
     public function __construct($nom, $prenom, $age, $dateNaissance, $ville){
         $this->_nom = $nom;
         $this->_prenom = $prenom;
-        $this->_age = $age;
         $this->_dateNaissance = $dateNaissance;
         $this->_ville = $ville;
     }
 
 
 }
+
+public function getNom(){
+        return $this->_nom;
+    }
+
+    public function getPrenom(){
+        return $this->_prenom;
+    }
+
+    public function getDateNaissance(){
+        return $this->_dateNaissance;
+    }
+
+    public function getVille(){
+        return $this->_ville;
+    }
+
+    
+    public function setNom($nom){
+        $this->_nom = $nom;
+    }
+
+    public function setPrenom($prenom){
+        $this->_prenom = $prenom;
+    }
+
+
+    public function setDateNaissance($dateNaissance){
+        $this->_dateNaissance = $dateNaissance;
+    }
+
+    public function setVille($ville){
+        $this->_ville = $ville;
+    }

5e98726b3579de577a32e74eaff75723b96a5070
Ennassmhammed <ennassirimohammed14@gmail.com>
2025-11-04 16:29:41 +0100

    exercice3

---
+++ exercice3.php
@@ -0,0 +1,173 @@
+<?php
+
+Class Voiture {
+    private string $_marque;
+    private string $_modele;
+  
+
+
+    public function __construct($marque, $modele, $nbPortes, int $vitesseActuelle){
+        $this->_marque = $marque;
+        $this->_modele = $modele; 
+      
+
+}
+public function __toString() {
+    {
+        return $this->_marque.' '.$this->_modele;
+    }
+}      
+public function getMarque(){                                     
+    return $this->_marque;
+}
+
+
+public function setMarque($marque){      
+    $this->_marque = $marque;
+}
+
+public function setModele($modele){
+    $this->_modele = $modele;
+
+}
+}
+
+
+<?php
+
+Class Voiture {
+    private string $_marque;
+    private string $_modele;
+  
+
+
+    public function __construct($marque, $modele, $nbPortes, int $vitesseActuelle){
+        $this->_marque = $marque;
+        $this->_modele = $modele; 
+      
+
+}
+public function __toString() {
+    {
+        return $this->_marque.' '.$this->_modele;
+    }
+}      
+public function getMarque(){                                     
+    return $this->_marque;
+}
+
+
+public function setMarque($marque){      
+    $this->_marque = $marque;
+}
+
+public function setModele($modele){
+    $this->_modele = $modele;
+
+}
+}
+
+
+<?php
+
+Class Voiture {
+    private string $_marque;
+    private string $_modele;
+  
+
+
+    public function __construct($marque, $modele, $nbPortes, int $vitesseActuelle){
+        $this->_marque = $marque;
+        $this->_modele = $modele; 
+      
+
+}
+public function __toString() {
+    {
+        return $this->_marque.' '.$this->_modele;
+    }
+}      
+public function getMarque(){                                     
+    return $this->_marque;
+}
+
+
+public function setMarque($marque){      
+    $this->_marque = $marque;
+}
+
+public function setModele($modele){
+    $this->_modele = $modele;
+
+}
+}
+$voiture1 = new Voiture("Peugeot", "408"); 
+
+
+
+
+
+
+
+Class Voiture {
+    private string $_marque;
+    private string $_modele;
+    private string $_autonomie;
+  
+
+
+    public function __construct($marque, $modele,){
+        $this->_marque = $marque;
+        $this->_modele = $modele; 
+        $this->_modele = $modele;
+      
+
+}
+public function __toString() {
+    {
+        return $this->_marque.' '.$this->_modele.' '.$this->autonomie;
+    }
+}      
+public function getMarque(){                                     
+    return $this->_marque;
+}
+public function getAutonomie(){                                     
+    return $this->_autonomie;
+}
+
+
+public function setMarque($marque){      
+    $this->_marque = $marque;
+}
+
+public function setModele($modele){
+    $this->_modele = $modele;
+
+}
+public function setAutonomie($autonomie){
+    $this->_autonomie = $autonomie;
+
+}
+$voiture1 = new Voiture("BMW", i3 150); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 

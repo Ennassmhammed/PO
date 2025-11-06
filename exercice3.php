f0e25b4debcaf4d4da0bc45be2a0a2b0adbe16fe
Ennassmhammed <ennassirimohammed14@gmail.com>
2025-11-06 10:56:32 +0100

    recorrection de lexercice voiture

--- exercice3.php
+++ exercice3.php
@@ -1,86 +1,66 @@
 <?php
 
-Class Voiture {
+class Voiture {
     private string $_marque;
     private string $_modele;
-  
-
 
     public function __construct($marque, $modele){
         $this->_marque = $marque;
-        $this->_modele = $modele; 
-      
-
-}
-public function __toString() {
-    {
-        return $this->_marque.' '.$this->_modele;
+        $this->_modele = $modele;
     }
-}      
-public function getMarque(){                                     
-    return $this->_marque;
-}
 
+    public function __toString() {
+        return $this->_marque . ' ' . $this->_modele;
+    }
 
-public function setMarque($marque){      
-    $this->_marque = $marque;
-}
+    public function getMarque(){
+        return $this->_marque;
+    }
 
-public function setModele($modele){
-    $this->_modele = $modele;
+    public function setMarque($marque){
+        $this->_marque = $marque;
+    }
 
-}
-public function getInfos() {
+    public function setModele($modele){
+        $this->_modele = $modele;
+    }
+
+    public function getInfos() {
         return "Voiture : " . $this->_marque . " " . $this->_modele;
     }
-
 }
 
+class VoitureElec extends Voiture {
+    private $_autonomie;
 
-Class Voiture elec {
-    private string $_marque;
-    private string $_modele;
-    private string $_autonomie;
-  
-
-
- public function __construct($marque, $modele) {
-        parent::__construct($marque, $modele,$autonomie); 
-        $this->_autonomie = $autonomie; 
-
-}
-public function __toString() {
-    {
-        return $this->_marque.' '.$this->_modele;
-          parent::__toString($marque, $modele); 
-        $this->_autonomie = $autonomie; 
+    public function __construct($marque, $modele, $autonomie) {
+        parent::__construct($marque, $modele);
+        $this->_autonomie = $autonomie;
     }
-}      
 
+    public function getAutonomie(){
+        return $this->_autonomie;
+    }
 
-public function getAutonomie(){                                     
-    return $this->_autonomie;
+    public function setAutonomie($autonomie){
+        $this->_autonomie = $autonomie;
+    }
+
+    public function getInfos() {
+        return "Voiture électrique : " . $this->getMarque() . " " . $this->_modele . " " . $this->_autonomie . " km";
+    }
+
+    public function __toString() {
+        return parent::__toString() . " - " . $this->_autonomie . " km";
+    }
 }
 
-
-public function setMarque($marque){      
-    $this->_marque = $marque;
-}
-
-public function setModele($modele){
-    $this->_modele = $modele;
-
-}
-public function setAutonomie($autonomie){
-    $this->_autonomie = $autonomie;
-
-}
-}
 $v1 = new Voiture("Peugeot", "408");
-$ve1 = new VoitureElec("BMW", "i3", 100); 
+$ve1 = new VoitureElec("BMW", "i3", 100);
 
 echo $v1->getInfos() . "<br/>";
 echo $ve1->getInfos() . "<br/>";
+
 
 
 
